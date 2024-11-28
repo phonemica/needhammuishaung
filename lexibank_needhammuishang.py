bed97024c14504fb30fdec1d271139e796c56a50
lingulist <mattis.list@lingulist.de>
2024-11-28 20:54:17 +0100

    update

--- lexibank_needhammuishang.py
+++ lexibank_needhammuishang.py
@@ -36,14 +36,14 @@ class Dataset(BaseDataset):
         # add concept
         concepts = {}
         for concept in self.concepts:
-            idx = concept["NUMBER"] + "_" + slug(concept["GLOSS"])
+            idx = concept["NUMBER"] + "_" + slug(concept["ENGLISH"])
             args.writer.add_concept(
                     ID=idx,
-                    Name=concept["GLOSS"],
+                    Name=concept["ENGLISH"],
                     Concepticon_ID=concept["CONCEPTICON_ID"],
                     Concepticon_Gloss=concept["CONCEPTICON_GLOSS"]
                     )
-            concepts[concept["NUMBER"]] = idx # lookup key
+            concepts[concept["LEXIBANK_GLOSS"]] = idx # lookup key
         args.log.info("added concepts")
 
         # add language
@@ -55,21 +55,30 @@ class Dataset(BaseDataset):
             "Needham-1897-264.tsv", delimiter="\t", dicts=True
         )
         # add data
+        errors = set()
         for entry in pb(data, desc="cldfify", total=len(data)):
-            args.writer.add_forms_from_value(
-                Language_ID="MuishaungNeedham",
-                Parameter_ID=concepts[entry["NUMBER"]],
-                Orthographic_Entry=entry["ORTH_NEEDHAM"],
-                Value=entry["PHON_NEEDHAM"],
-                Source=["Needham1897"],
-                Attestation=1897
-            )
-            if entry["PHON_MODERN"].strip() and entry["PHON_MODERN"] not in ["--"]:
+            if entry["GLOSS"] not in concepts:
+                errors.add(entry["GLOSS"])
+            else:
                 args.writer.add_forms_from_value(
+                    Language_ID="MuishaungNeedham",
+                    Parameter_ID=concepts[entry["GLOSS"]],
+                    Orthographic_Entry=entry["ORTH_NEEDHAM"],
+                    Value=entry["PHON_NEEDHAM"],
+                    Source=["Needham1897"],
+                    Attestation=1897
+                )
+            if entry["PHON_MODERN"].strip() and entry["PHON_MODERN"] not in ["--"]:
+                if entry["GLOSS"] not in concepts:
+                    errors.add(entry["GLOSS"])
+                else:
+                    args.writer.add_forms_from_value(
                     Language_ID="MuishaungModern",
-                    Parameter_ID=concepts[entry["NUMBER"]],
+                    Parameter_ID=concepts[entry["GLOSS"]],
                     Orthographic_Entry=entry["ROMAN_MODERN"],
                     Value=entry["PHON_MODERN"],
                     Source="VanDam2025",
                     Attestation=2024
                     )
+        for error in errors:
+            print(error)
